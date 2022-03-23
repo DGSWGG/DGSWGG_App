757851c6a6b0021dd72b723d5cd040384ab5763e
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-23 15:40:11 +0900

    [ADD] Create SplashAPI Splash

--- presentation/src/main/java/kr/hs/dgsw/presentation/ui/activity/MainActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/presentation/ui/activity/MainActivity.kt
@@ -2,11 +2,14 @@ package kr.hs.dgsw.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import kr.hs.dgsw.presentation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
 }

ad322e4787f1a73619abb74fd4fbc18f7d7a551b
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-23 14:24:15 +0900

    [FIX] Activity 위치 변경

--- presentation/src/main/java/kr/hs/dgsw/presentation/ui/activity/MainActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/presentation/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.hs.dgsw.presentation
+package kr.hs.dgsw.presentation.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.hs.dgsw.presentation.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

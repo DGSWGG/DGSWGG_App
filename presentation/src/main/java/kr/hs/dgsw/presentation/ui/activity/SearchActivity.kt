85a938e00290a0ebae1708b5ddff834ccbbc6e15
양윤재 <diddbswo04@dgsw.hs.kr>
2022-03-25 10:22:03 +0900

    [ADD] Create SearchActivity Binding

--- presentation/src/main/java/kr/hs/dgsw/presentation/ui/activity/SearchActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/presentation/ui/activity/SearchActivity.kt
@@ -3,10 +3,15 @@ package kr.hs.dgsw.presentation.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.hs.dgsw.presentation.R
+import kr.hs.dgsw.presentation.databinding.ActivitySearchBinding
+import kr.hs.dgsw.presentation.util.bindings
 
 class SearchActivity : AppCompatActivity() {
+
+    private val binding: ActivitySearchBinding by bindings(ActivitySearchBinding::inflate)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        setContentView(binding.root)
     }
 }

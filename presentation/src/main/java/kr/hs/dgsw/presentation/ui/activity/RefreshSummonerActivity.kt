c81742da2fabf485e1d396b8aa1a0bf6ac4e83e2
양윤재 <diddbswo04@dgsw.hs.kr>
2022-04-15 15:03:43 +0900

    [ADD] Create FloatingActionButton: Navigate To Refresh Summoner Info Page

---
+++ presentation/src/main/java/kr/hs/dgsw/presentation/ui/activity/RefreshSummonerActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.dgsw.presentation.R
+
+class RefreshSummonerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_refresh_summoner)
+    }
+}
